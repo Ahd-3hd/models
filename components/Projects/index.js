2c790f608d9595b23d806227613469445cc7d587
ahd <ahdhani0@gmail.com>
2021-02-27 18:14:44 +0300

    create projects slider

--- components/Projects/index.js
+++ components/Projects/index.js
@@ -1,11 +1,101 @@
-import { Wrapper, ScrollButton } from "./index.style";
-import { memo, useContext } from "react";
+import {
+  Wrapper,
+  ScrollButton,
+  Title,
+  SliderControlsContainer,
+  SliderCaret,
+  SliderCircle,
+  Container,
+  InnerContainer,
+  Slide,
+  Image,
+  ProjectDetails,
+  ProjectButtonsContainer,
+  ProjectButton,
+  ProjectTitle,
+  ProjectDescription,
+} from "./index.style";
+import { memo, useContext, useState } from "react";
 import ScrollContext from "../../context/scrollContext";
+// import CodeIcon from "../../svg/CodeIcon.svg";
+// import VisitIcon from "../../svg/VisitIcon.svg";
+import Caret from "../../svg/Caret.svg";
 
 const Projects = () => {
   const { page, setPage } = useContext(ScrollContext);
+  const [slide, setSlide] = useState(0);
   return (
     <Wrapper page={page}>
+      <Title>
+        Projects <span>&</span> Experiments
+      </Title>
+      <Container>
+        <InnerContainer slide={slide}>
+          <Slide>
+            <Image src="https://picsum.photos/900/800" />
+            <ProjectDetails>
+              <ProjectTitle>Project Name</ProjectTitle>
+              <ProjectDescription>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo
+              </ProjectDescription>
+              <ProjectButtonsContainer>
+                <ProjectButton>Github</ProjectButton>
+                <ProjectButton>Visit</ProjectButton>
+              </ProjectButtonsContainer>
+            </ProjectDetails>
+          </Slide>
+          <Slide>
+            <Image src="https://picsum.photos/900/800" />
+            <ProjectDetails>
+              <ProjectTitle>Project Name</ProjectTitle>
+              <ProjectDescription>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo
+              </ProjectDescription>
+              <ProjectButtonsContainer>
+                <ProjectButton>Github</ProjectButton>
+                <ProjectButton>Visit</ProjectButton>
+              </ProjectButtonsContainer>
+            </ProjectDetails>
+          </Slide>
+          <Slide>
+            <Image src="https://picsum.photos/900/800" />
+            <ProjectDetails>
+              <ProjectTitle>Project Name</ProjectTitle>
+              <ProjectDescription>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo
+              </ProjectDescription>
+              <ProjectButtonsContainer>
+                <ProjectButton>Github</ProjectButton>
+                <ProjectButton>Visit</ProjectButton>
+              </ProjectButtonsContainer>
+            </ProjectDetails>
+          </Slide>
+        </InnerContainer>
+        <SliderControlsContainer>
+          <SliderCaret onClick={() => setSlide((prev) => prev > 0 && prev - 1)}>
+            <Caret />
+          </SliderCaret>
+          <SliderCircle />
+          <SliderCircle />
+          <SliderCircle />
+          <SliderCircle />
+          <SliderCaret
+            flip
+            onClick={() => setSlide((prev) => prev < 2 && prev + 1)}
+          >
+            <Caret />
+          </SliderCaret>
+        </SliderControlsContainer>
+      </Container>
       <ScrollButton onClick={() => setPage(3)}>Scroll</ScrollButton>
     </Wrapper>
   );
--- components/Projects/index.style.js
+++ components/Projects/index.style.js
@@ -11,7 +11,79 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: fit-content;
+  @media (min-width: 1024px) {
+    align-items: flex-start;
+    padding: 10vh 3vw;
+  }
+`;
+
+export const Title = styled.h3`
+  color: #353535;
+  font-size: 1.5rem;
+  margin: 0;
+  > span {
+    color: #1f6eb7;
+  }
+  @media (min-width: 1024px) {
+    font-size: 2.5rem;
+  }
+`;
+
+export const ProjectButton = styled.a`
+  background: #353535;
+  color: #ffffff;
+  border-radius: 0.2rem;
+  box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.15);
+  text-align: center;
+  height: 2.5rem;
+  width: 40%;
+  display: flex;
+  > svg {
+    width: 1.3rem;
+    fill: #ffffff;
+  }
+  align-items: center;
+  justify-content: center;
+  @media (min-width: 1024px) {
+    max-width: 150px;
+    margin-right: 1rem;
+  }
+`;
+export const ProjectButtonsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 2rem;
+  @media (min-width: 1024px) {
+    justify-content: flex-start;
+  }
+`;
+
+export const SliderControlsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1.5rem;
+  @media (min-width: 1024px) {
+    justify-content: flex-start;
+  }
+`;
+export const SliderCaret = styled.button`
+  background: transparent;
+  border: none;
+
+  > svg {
+    width: 0.6rem;
+    fill: #353535;
+    transform: ${({ flip }) => flip && "rotateY(180deg);"};
+  }
+`;
+export const SliderCircle = styled.button`
+  width: 1rem;
+  height: 1rem;
+  border-radius: 1rem;
+  border: none;
+  margin: 0 0.5rem;
+  background: #35353555;
 `;
 
 export const ScrollButton = styled.button`
@@ -25,4 +97,69 @@ export const ScrollButton = styled.button`
   border-radius: 0.3rem;
   color: #333333;
   font-size: 1rem;
+`;
+
+export const Container = styled.div`
+  overflow: hidden;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const InnerContainer = styled.div`
+  display: flex;
+  transform: translateX(${({ slide }) => -slide * 100}%);
+  transition: 0.5s ease-in-out;
+`;
+
+export const Slide = styled.div`
+  min-width: 100%;
+  padding: 0 1rem;
+  display: flex;
+  flex-direction: column;
+  /* overflow: hidden; */
+  @media (min-width: 1024px) {
+    flex-direction: row-reverse;
+  }
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  /* max-width: 800px; */
+  max-height: 50vh;
+  box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.15);
+  object-fit: cover;
+  object-position: center;
+  @media (min-width: 1024px) {
+    width: 50%;
+  }
+`;
+
+export const ProjectDetails = styled.div`
+  @media (min-width: 1024px) {
+    width: 50%;
+
+    margin-right: 2rem;
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export const ProjectTitle = styled.p`
+  font-size: 2rem;
+  font-weight: 700;
+  color: #1f6eb7;
+  display: none;
+  @media (min-width: 1024px) {
+    display: block;
+  }
+`;
+export const ProjectDescription = styled.p`
+  line-height: 150%;
+  color: #353535;
+  display: none;
+  @media (min-width: 1024px) {
+    display: block;
+  }
 `;
