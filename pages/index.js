3f8ccd1a8dee3c2757bedca294f4b3b950dbfab0
ahd <ahdhani0@gmail.com>
2021-02-28 16:28:01 +0300

    add wheel event listener for scrolling

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,36 @@ import Projects from "../components/Projects";
 import { useState } from "react";
 import { Wrapper } from "../styles/Home.styles";
 import ScrollContext from "../context/scrollContext";
+import { useGesture, useWheel } from "react-use-gesture";
+import { Lethargy } from "lethargy";
 
 const World = dynamic(import("../World"), { ssr: false });
+const lethargy = new Lethargy();
 
 export default function Home() {
   const [page, setPage] = useState(0);
+  const [scrollEnabled, setScrollEnabled] = useState(true);
+
+  const bind = useWheel(({ event, last, first, memo: wait = false }) => {
+    if (first && scrollEnabled) {
+      const s = lethargy.check(event);
+      if (s < 0 && page < 3) {
+        setPage((prev) => prev + 1);
+        setScrollEnabled(false);
+        setTimeout(() => setScrollEnabled(true), 1900);
+      } else if (s > 0 && page > 0) {
+        setPage((prev) => prev - 1);
+        setScrollEnabled(false);
+        setTimeout(() => setScrollEnabled(true), 1900);
+      }
+    } else {
+      return false;
+    }
+  });
 
   return (
     <ScrollContext.Provider value={{ page: page, setPage: setPage }}>
-      <Wrapper>
+      <Wrapper {...bind()}>
         <World />
         <Header />
         <About />
